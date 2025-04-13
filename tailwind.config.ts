e67b1203fb4becd7e05a986b88b343b5bf062cb7
aleiiin <aleks_aleks.301006@mail.ru>
2025-04-13 20:47:38 +0300

    Update website with new changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,6 +84,10 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        'sans-bold': ['Montserrat', 'sans-serif'],
+      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
